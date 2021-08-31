dddd0fd8e8cb9823509fa1c72b36aea928430999
Dang Cuong <cuongcesc12a1@gmail.com>
2021-08-31 22:25:53 +0700

    vd7

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import Header from "./component/header";
 import Product from "./component/product";
 
 function App() {
+let products = [
+{id:1,name:"xiaomi mi 10 lite",price:"9.000.000d",status:true},
+{id:2,name:"xiaomi mi mix 4",price:"16.000.000d",status:true},
+{id:3,name:"xiaomi mi mix 3",price:"10.000.000d",status:false},
+{id:4,name:"xiaomi redmi note 10 pro",price:"7.000.000d",status:true},
+];
+let elements = products.map((product,index) => {
+return <Product key="index" id={product.id} price={product.price} status={product.status}>
+  {product.name}
+</Product>
+});
 return (
 <div>
   <Header />
   <div className="row">
     <div className="col-12">
-      <Product />
+      {elements}
     </div>
   </div>
 </div>
--- src/component/product.js
+++ src/component/product.js
@@ -1,21 +1,5 @@
 function Product() {
-let products = [
-{id:1,name:"xiaomi mi 10 lite",price:"9.000.000d",status:true},
-{id:2,name:"xiaomi mi mix 4",price:"16.000.000d",status:true},
-{id:3,name:"xiaomi mi mix 3",price:"10.000.000d",status:false},
-{id:4,name:"xiaomi redmi note 10 pro",price:"7.000.000d",status:true},
-];
-let elements = products.map((product,index) => {
-return <tr key="index">
-    <td>{product.id}</td>
-    <td>{product.name}</td>
-    <td>{product.price}</td>
-    <td>{product.status === true ? 'Còn hàng' : 'Hết hàng'}</td>
-</tr>
-});
 return (
-
-
 <div>
     <div class="container">
 
@@ -29,7 +13,15 @@ return (
                 </tr>
             </thead>
             <tbody>
-                {elements}
+                <div>
+                    <tr>
+                        <td>{this.props.id}</td>
+                        <td>{this.props.children}</td>
+                        <td>{this.props.price}</td>
+                        <td>{this.props.status === true ? 'Còn hàng' : 'Hết hàng'}</td>
+                    </tr>
+                </div>
+
             </tbody>
         </table>
 
