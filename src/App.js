6b176ea584e263b3584ebc3801100c421f43cf9b
Dang Cuong <cuongcesc12a1@gmail.com>
2021-09-02 00:33:58 +0700

    html to reactjs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,86 @@
-// import logo from "./logo.svg";
 import "./App.css";
-import Header from "./component/header";
-import Product from "./component/product";
+// import React, { Component } from "react";
+import TopMenu from "./component/Header/TopMenu";
+import Banner from "./component/Header/Banner";
+import Section1 from "./component/Section/Section1";
+import  Section2 from "./component/Section/Section2";
+import Section3 from "./component/Section/Section3";
+import Footer from "./component/Footer/Footer";
+// class Four extends Component {
+// render() {
+// return (
+// <div>
+//   <button>React component</button>
+// </div>
+// );
+// }
+// }
+
+// function DemoProps(props){
+// return <div>
+
+//   <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+//     <div className="thumbnail">
+//       <img src={props.img} alt={props.title} />
+//       <div className="caption">
+//         <h3>{props.title}</h3>
+//         <p>
+//           {props.content}
+//         </p>
+//         <p>
+//           <a href="!#" className="btn btn-primary">Action</a>
+//           <a href="!#" className="btn btn-default">Action</a>
+//         </p>
+//       </div>
+//     </div>
+//   </div>
+
+// </div>
+// }
+
+// class DemoCl extends Component {
+//   render() {
+//     return (
+//       <div>
+          
+//           <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+//             <div className="thumbnail">
+//               <img src={this.props.img} alt=""/>
+//               <div className="caption">
+//                 <h3>{this.props.title}</h3>
+//                 <p>
+//                   {this.props.content}
+//                 </p>
+//                 <p>
+//                   <a href="!#" className="btn btn-primary">Action</a>
+//                   <a href="!#" className="btn btn-default">Action</a>
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+          
+//       </div>
+//     );
+//   }
+// }
+
 
 function App() {
-let products = [
-{id:1,name:"xiaomi mi 10 lite",price:"9.000.000d",status:true},
-{id:2,name:"xiaomi mi mix 4",price:"16.000.000d",status:true},
-{id:3,name:"xiaomi mi mix 3",price:"10.000.000d",status:false},
-{id:4,name:"xiaomi redmi note 10 pro",price:"7.000.000d",status:true},
-];
-let elements = products.map((product,index) => {
-return <Product key="index" id={product.id} price={product.price} status={product.status}>
-  {product.name}
-</Product>
-});
 return (
 <div>
-  <Header />
-  <div className="row">
-    <div className="col-12">
-      {elements}
-    </div>
-  </div>
+  <TopMenu />
+  <Banner />
+  <Section1 />
+  <Section2 />
+  <Section3 />
+  <Footer />
+  {/* <Four /> */}
+  {/* <DemoProps img="https://d25tv1xepz39hi.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg"
+    title="Landscape" content="Landscape Demo" />
+    <DemoCl img="https://d25tv1xepz39hi.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg"
+    title="Landscape" content="Landscape Demo" />
+    <DemoCl img="https://d25tv1xepz39hi.cloudfront.net/2017-08-21/files/landscape-photography_1645.jpg"
+    title="Landscape" content="Landscape Demo" /> */}
 </div>
 );
 }
